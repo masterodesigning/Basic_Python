c409c10f90cb6eab1d90c991c6f2375d739de0a1
Kiaan <marutyprem@gmail.com>
2026-09-17 14:01:02 +0500

    Added firstprogram.py

---
+++ firstprogram.py
@@ -0,0 +1,18 @@
+name = "Durga"         # Variable Declearation
+age = 18
+
+print(name)            # Printing statement 
+print(age),
+print(address)
+# print(type(name))    # For Checking Data Type For Name
+# print(type(age))     # For Checking Data Type For Age
+                       # Note Important to Write in the program only check data type if you want
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
